deedb0a6764fbfa15f202b500db33f1f5b7f8832
JawaraGordon <jawaragordon@icloud.com>
2022-06-25 13:17:25 -0400

    completed assignment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 class ApplicationController < Sinatra::Base
+  require 'pry'
   set :default_content_type, 'application/json'
 
   get '/games' do
-    games = Game.all.order(:title).limit(10)
+    games = Game.all.order(:title)
     games.to_json
   end
 
@@ -14,6 +15,45 @@ class ApplicationController < Sinatra::Base
         user: { only: [:name] }
       } }
     })
+  end
+
+  get '/reviews' do
+    games = Game.all.order(:title)
+    games.to_json
+  end
+
+  get '/reviews/:id' do
+    reviews = Review.find(params[:id])
+    reviews.to_json
+  end
+
+
+  post '/reviews' do
+    review = Review.create(
+      score: params[:score],
+      comment: params[:comment],
+      game_id: params[:game_id],
+      user_id: params[:user_id]
+    )
+    review.to_json
+  end
+
+  patch '/reviews/:id' do
+    review = Review.find(params[:id])
+    review.update(
+      score: params[:score],
+      comment: params[:comment]
+    )
+    review.to_json
+  end
+
+  delete '/reviews/:id' do
+    # find the review using the ID
+  review = Review.find(params[:id])
+  # delete the review
+  review.destroy
+  # send a response with the deleted review as JSON
+  review.to_json
   end
 
 end
